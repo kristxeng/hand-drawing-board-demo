03931363cedd08e9b9a9037eb424f09eebbf30e6
kris <kristxeng@gmail.com>
2020-02-12 17:09:44 +0800

    feat: hand drawing board demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { fabric } from 'fabric';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'hand-drawing-board-demo';
+export class AppComponent implements AfterViewInit {
+  title = 'ng-fabric';
+
+  canvas: fabric.Canvas;
+
+  svgSrcUrl: SafeUrl = '';
+
+  constructor(
+    private sanitizer: DomSanitizer,
+  ) {}
+
+  ngAfterViewInit() {
+    this.canvas = new fabric.Canvas('canvas', { isDrawingMode: true });
+
+    // const rect = new fabric.Rect({
+    //   top : 100,
+    //   left : 100,
+    //   width : 60,
+    //   height : 70,
+    //   fill : 'red'
+    // });
+    // canvas.add(rect);
+  }
+
+  clear() {
+    this.canvas.clear();
+  }
+
+  toSVG() {
+    const svgString = this.canvas.toSVG();
+    console.log(svgString);
+
+    const url = this.getSvgFromString( svgString );
+    const sanitizedUrl = this.sanitizer.bypassSecurityTrustUrl( url );
+
+    this.svgSrcUrl = sanitizedUrl ;
+  }
+
+  getSvgFromString(svgString): string {
+    const blob = new Blob([svgString], {type: 'image/svg+xml'});
+    const url = URL.createObjectURL(blob);
+
+    return url;
+  }
 }
